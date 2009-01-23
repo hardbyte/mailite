0969ef3364eafd12b6e6f888278dc3db427ee437
hardbyte <hardbyte@6996a418-bb80-11dd-871e-457e4afae058>
2009-01-23 04:31:19 +0000

    Changed to have a seperate config file. So I can work directly with svn copy.

---
+++ config.py
@@ -0,0 +1,4 @@
+database_host = "localhost"
+database_name = ""
+database_user = ""    # user needs at least read access.
+database_passwd = ""
--- mailite.py
+++ mailite.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-""" 
+"""
 Mailite - to re-send emails to users in a database.
 
 Typical usage:
@@ -27,16 +27,7 @@ TODO:
 * add groups - simple mailing lists
 * make a mqs function - connect to db and select a single item and return it...
 """
-
-
-###############################################################################
-# Customize these settings to suit your needs:
-###############################################################################
-database_host = "localhost"
-database_name = ""
-database_user = ""    # user needs at least read access.
-database_passwd = ""
-
+from config import *
 name_tb = "members"             #the table in the database that contains names
 name_tb_name = "Name"           #the field in name_table that contains all the names 
 name_tb_id = "mid"              #if the member is refered to by a number enter the field found on the name table here
@@ -47,12 +38,30 @@ email_table_id = name_tb_id     #the field to link emails to names (used if data
 email_table_filter = ""
 email_tb_email = "eMail"        #the field name for email addresses in the email_table
 
-group_tb = "officers"           #the table containing any jobs...
-group_tb_name = "Office"        #field to search through for job
-group_tb_email = "eMail"        #field in table for email NOTE: may actually be a memberID or something (CHANGE ME... later)
+redirect_jobs = True
+if redirect_jobs:
+    job_tb = "officers"           #the table containing any jobs...
+    job_tb_name = "Office"        #field to search through for job
+    job_tb_email = "eMail"        #field in table for email NOTE: may actually be a memberID or something (CHANGE ME... later)
 
-loggingDir = "/home/bevs/bin/"
+redirect_groups = True
+if redirect_groups:    
+    #Group redirects are for messaging lists of people
+    #Currently set up for a table of mapping each user to each group.
+    group_redirects = True
+    groups_tb = "groups"
+    groups_tb_index = "gid"         #Group ID, set to None if map is done on title alone.
+    groups_tb_title = "name"        #field to search through for group title.
+
+    #Table that maps line by line user (by ID) to group (by ID)
+    groupmap_tb = "group_map"
+    groupmap_tb_group_index = "gid"
+    groupmap_tb_member_index = "mid"
+
+loggingDir = "./"
 logFileName = loggingDir + "mailite.log"
+
+save_emails_on_server = False
 wildcard = "%"                  #the character or string used as a wildcard by your database 
 
 ###############################################################################
@@ -61,6 +70,7 @@ wildcard = "%"                  #the character or string used as a wildcard by y
 import sys,logging,time
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(filename)s %(levelname)s %(message)s',filename=logFileName)
 logging.info("Starting mailite script")
+
 
 class NameNotFound(Exception):
     pass
@@ -75,11 +85,23 @@ def mqs(query):
     try:
         cursor.execute(query)
         row = cursor.fetchone()
-        if row is not None: return row[0]
-        return row
+        if len(row) > 1:
+            return row[0]
+        else:
+            return row
     except Exception, e:
         logging.error('error occured in mysql query. \nQuery:%s \n Error: %s' % (query,e))
         raise SystemExit
+
+def splitEmail(email):
+    """split up the email address into sections before and after the @ symbol"""
+    try:
+        section = email.split('@')[0]
+        logging.debug("first section of email: %s" % section)
+    except Exception, e:
+        logging.error("failed to split email.")
+        raise EmailMalformed
+    return section
 
 def lookupUser(email):
     """
@@ -89,28 +111,36 @@ def lookupUser(email):
     """
     logging.debug("looking up email: %s" % email)
     try:
-        section = email.split('@')[0]
-        logging.debug("first section of email: %s" % section)
-    except Exception, e:
-        logging.error("failed to split email.")
-        raise EmailMalformed
-    try:
+        section = splitEmail(email)
         newEmail = emailFromName(section)
         assert newEmail is not None
+        logging.debug("Found replacement email address: %s" % newEmail)
+        return newEmail
     except:
         logging.debug("email was not a name")
+    if redirect_jobs:
         try:
             newEmail = emailFromJob(section)
             assert newEmail is not None
+            logging.debug("Job found, new email/s: %s" % newEmail)
+            return newEmail
         except:
-            logging.debug("email was not a job either")
-            print """
-            Hello,
-            We are sorry, but the email address (%s) was not found.
-            %s
-            """ % (section, email.split('@')[1])
-            raise SystemExit
-    return newEmail
+            logging.debug("email was not a job")
+    if redirect_groups:
+        try:
+            newEmail = emailFromGroup(section)
+            assert newEmail is not None
+            logging.debug("Group found, new email/s: %s" % newEmail)
+        except:
+            logging.debug("email was not a group")
+            
+    print """
+    Hello,
+    We are sorry, but the email address (%s) was not found.
+    %s
+    """ % (section, email.split('@')[1])
+    raise SystemExit
+
 
 def strReplace(s,old,new):
     """
@@ -132,13 +162,17 @@ def emailFromName(name):
     searchStr = wildcard + strReplace(name,"*.-_+1234567890",wildcard) + wildcard
     query = "SELECT " + email_tb_email + " FROM " + name_tb + " WHERE " + name_tb_name + ' LIKE "' + searchStr + '" LIMIT 1'
     return mqs(query)
-    
+
+
 def emailFromJob(job):
     """Given a job title find the email addy in a db or return None"""
     job = wildcard + strReplace(job,"*.-_+1234567890",wildcard) + wildcard
     
-    query = "SELECT " + group_tb_email + " FROM " + group_tb + " WHERE LOWER(" + group_tb_name + ') LIKE "' + job.lower() + '" LIMIT 1'
+    query = "SELECT " + job_tb_email + " FROM " + job_tb + " WHERE LOWER(" + job_tb_name + ') LIKE "' + job.lower() + '" LIMIT 1'
     return mqs(query)
+def emailFromGroup(group):
+    """Given a possible group name, search the database and return a list of addresses"""
+    return ["hardbyte+1@gmail.com","hardbyte+2@gmail.com"]
 
 def connectToDB():
     logging.debug("importing MySQLdb")
@@ -162,6 +196,27 @@ def connectToDB():
         raise SystemExit
     return (conn, cursor)
 
+
+def openEmailFromString(email_string):
+    logging.debug("parsing the email")
+    try:
+        import email
+        email_data = email.message_from_string(email_string)
+    except Exception, e:
+        logging.error("couldn't parse email...")
+        raise SystemExit
+    return email_data
+    
+def saveEmailToString():
+    logging.debug("reading the email that is being piped in...")
+    try:
+        raw_email = sys.stdin.read() #read the email from stdin
+        return raw_email
+    except Exception, e:
+        logging.error("Couldn't get email from std in...")
+        raise SystemExit
+
+
 def saveEmailToFile():
     logging.debug("first save the email that is being piped in...")
     try:
@@ -182,6 +237,7 @@ def saveEmailToFile():
         raise SystemExit
     return filename
 
+
 def openEmailFromFile(filename):
     logging.debug("open the saved email")
     try:
@@ -194,48 +250,59 @@ def openEmailFromFile(filename):
         raise SystemExit
     return email_data
 
+
 def redirectEmail(email_data):    
     logging.debug("changing the recipient address based on db lookup")
     try:
-        email_data.replace_header('To',lookupUser(email_data['To']))
+        emailAddys = lookupUser(email_data['To'])
+        if len(emailAddys) > 1:
+            emailAddys = ",".join(emailAddys)
+        
+        logging.debug("Addresses replaced with: %s" % emailAddys)    
+        email_data.replace_header('To',emailAddys)
     except:
         logging.error("Couldn't set new address.")
         raise SystemExit
     return email_data
+ 
     
 def sendEmail(email_data):
     logging.debug("about to send the email onwards...")
     try:
         import smtplib
+        logging.debug('imported smtplib')
+    except Exception, e:
+        logging.error("Couldn't import smtplib")
+    try:
         smtp_conn = smtplib.SMTP()
         smtp_conn.connect()
+        logging.debug("smtp connection setup")
+        #logging.debug("email from: %s, to: %s, msg: '%s'" % (email_data["From"],email_data['To'],email_data.as_string()))
         smtp_conn.sendmail(email_data['From'], email_data['To'], email_data.as_string())
-        logging.debug("email was sent... from: %s to: %s" % (email_data['From'],email_data['To']))
+        #logging.debug("email was sent... from: %s to: %s" % (email_data['From'],email_data['To']))
         smtp_conn.close()
     except Exception, e:
         logging.error("Couldn't send email")
+        logging.error("error: %s" % e)
         raise SystemExit    
+
 
 def closeConnection(conn,cursor):
     logging.debug("Closing Connection")        
     cursor.close()
     conn.commit()
     conn.close()
-    
 
 conn, cursor = connectToDB()
-filename = saveEmailToFile()
-email_data = openEmailFromFile(filename)
+if save_emails_on_server:
+    logging.info("Emails will be saved to a file and left on server.")
+    filename = saveEmailToFile()
+    email_data = openEmailFromFile(filename)
+else:
+    logging.info("Emails will be parsed in memory and not stored.")
+    emailString = saveEmailToString()
+    email_data = openEmailFromString(emailString)
+
 new_email_data = redirectEmail(email_data)
 sendEmail(new_email_data)
 closeConnection(conn,cursor)
-
-
-#version = mqs("SELECT VERSION()")
-#logging.info("MySql Version is: %s" % version)
-
-#cursor.execute ("SELECT name, eMail FROM members")
-#rows = cursor.fetchall()
-#for row in rows:
-#    logging.debug("%s, %s" % row)
-#logging.debug("Number of rows returned: %d" % cursor.rowcount)
